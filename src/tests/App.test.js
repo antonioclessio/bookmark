0825ea2b24d16ac1328cd43023123389fff9ed6a
Antonio Clessio de Sousa Vieira <clessio@outlook.com>
2017-07-06 21:44:34 -0300

    Desenvolvendo unit test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -36,7 +36,84 @@ describe('Redux', () => {
 
             const verificacao = dataSourceStructure(stateInitial, action);
  
-            assert.deepEqual(verificacao, verificacao, "Falha ao adicionar");
+            assert.deepEqual(verificacao, expected, "Falha ao adicionar");
+        });
+    });
+
+    describe('DEL_TAG', () =>{
+        it('Removendo item', () =>{
+            const stateInitial = [{
+                id: 1,
+                title: "Google",
+                url: "http://www.google.com",
+                tags: ["Search", "Web"]
+            },
+            {
+                id: 2,
+                title: "Facebook",
+                url: "http://www.facebook.com",
+                tags: ["Network", "Web"]
+            }];
+
+            const action = {
+                type: 'DEL_TAG',
+                tag: 'Web',
+                dataItem: {
+                    id: 2,
+                    title: "Facebook",
+                    url: "http://www.facebook.com",
+                    tags: ["Network", "Web"]
+                }
+            };
+
+            const expected = [{
+                id: 1,
+                title: "Google",
+                url: "http://www.google.com",
+                tags: ["Search", "Web"]
+            }];
+
+            const verificacao = dataSourceStructure(stateInitial, action);
+ 
+            assert.deepEqual(verificacao, expected, "Falha ao remover a tag");
+        });
+    });
+
+    describe('DEL', () =>{
+        it('Removendo item', () =>{
+            const stateInitial = [{
+                id: 1,
+                title: "Google",
+                url: "http://www.google.com",
+                tags: ["Search", "Web"]
+            },
+            {
+                id: 2,
+                title: "Facebook",
+                url: "http://www.facebook.com",
+                tags: ["Network", "Web"]
+            }];
+
+            const action = {
+                type: 'DEL',
+                dataItem: {
+                    id: 2,
+                    title: "Facebook",
+                    url: "http://www.facebook.com",
+                    tags: ["Network", "Web"]
+                }
+            };
+
+            const expected = [{
+                id: 1,
+                title: "Google",
+                url: "http://www.google.com",
+                tags: ["Search", "Web"]
+            }];
+
+            const verificacao = dataSourceStructure(stateInitial, action);
+ 
+            assert.deepEqual(verificacao, expected, "Falha ao remover o item");
         });
     });
 });
